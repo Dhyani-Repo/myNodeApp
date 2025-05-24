a25c9d6219215d9e479c54edf62dbe6d4f856d29
= <devender.dhyani@thewitslab.com>
2025-05-24 19:29:08 +0530

    feat/redis implemented followed by singleton pattern

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import express, {Request, Response,Express} from 'express'
 import * as dotenv from 'dotenv';
-import rout from './src/router/main.router';
+import {MainRouter} from './src/router/main.router';
 
 dotenv.config()
 const port = process.env.PORT || 3000
@@ -11,10 +11,13 @@ declare namespace Express {
   }
 }
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true}));
 
-app.use("/api/v1", rout)
+(async () => {
+  const routerMain = await MainRouter.init()
+  app.use("/api/v1", routerMain.getRouter());
 
-app.listen(port,() => {
-    console.log("Server is running on port: ",port)
-})
+  app.listen(port, () => {
+    console.log("Server is running on port:", port);
+  });
+})();
--- src/router/main.router.ts
+++ src/router/main.router.ts
@@ -4,12 +4,39 @@ import { EMainGender } from "../types/main.types";
 import { authMiddleware } from "../middleware/main.authMiddleware";
 import { validateSchema } from "../middleware/validateSchema.middleware";
 import { RegisterSchema } from "../validations/joy/register.schema";
-const rout = Router()
-const contoller = new MainController()
+import { RedisClientSingleton } from "../utils/redis";
+// export const getMainRouter =  async() => {
+//   const rout = Router();
+//   const controller = await MainController.init();
 
+//   rout.post("/user/create", validateSchema(RegisterSchema), controller.register);
+//   rout.post("/user/login", controller.login);
+//   rout.post("/refresh", controller.refreshToken);
+//   rout.get("/user/:id", authMiddleware, controller.getUserById);
 
-rout.post("/user/create",validateSchema(RegisterSchema),contoller.register)
-rout.post("/user/login",contoller.login)
-rout.post("/refresh",contoller.refreshToken)
-rout.get("/user/:id",authMiddleware,contoller.getUserById)
-export default rout
+//   return rout;
+// };
+export class MainRouter {
+
+  private controller: MainController;
+
+  private constructor(controller: MainController) {
+    this.controller = controller;
+  }
+
+  public static init = async () => {
+    const mainController = await MainController.init();
+    const mainRouter = new MainRouter(mainController)
+    return mainRouter
+  }
+  
+  public getRouter = () => {
+    const router = Router();
+    router.post("/user/create", validateSchema(RegisterSchema), this.controller.register.bind(this.controller));
+    router.post("/user/login", this.controller.login);
+    router.post("/refresh", this.controller.refreshToken);
+    router.get("/user/:id", authMiddleware, this.controller.getUserById);
+    return router;
+  }
+
+}
